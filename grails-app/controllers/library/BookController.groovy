f3388e3381247942fedf2ea064d1d485202ef412
Rohan <rohan@localhost.localdomain>
2016-12-12 11:33:35 +0530

    added view for book

--- grails-app/controllers/library/BookController.groovy
+++ grails-app/controllers/library/BookController.groovy
@@ -11,7 +11,7 @@ class BookController extends RestfulController {
     }
     @Transactional(readOnly = true)
     def index() {
-        respond Book.list()
+        respond Book.list([fetch: [author: "join"]])
     }
     @Transactional
     def save(Book book) {
